ff18810cb76f9c4574052ce9a88bba5a96c9036c
Andy <amillion3@gmail.com>
2018-09-28 15:54:34 -0500

    added single user and posts functionality

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,7 +8,16 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  // get all users
   getUsers() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+  // get a single user
+  getUser(userId) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users/' + userId);
+  }
+  // gets fictional user posts
+  getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
   }
 }
